5e0e03ddedf48e7067f7ff9b78ab89575c81eabe
preciouStar <seongjin605@users.noreply.github.com>
2020-04-13 00:28:25 +0900

    Account 테스트 코드 작성

---
+++ src/test/java/com/sns/server/index/IndexControllerTest.java
@@ -0,0 +1,23 @@
+package com.sns.server.index;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 인덱스_페이지_로드() {
+        String body = restTemplate.getForObject("/", String.class);
+        assertThat(body).contains("swagger-ui");
+    }
+}
