c9625c54f30ef410f481330329e9c30c33e69f2b
preciouStar <seongjin605@users.noreply.github.com>
2020-04-15 22:32:29 +0900

    Reply domain 기본 컬럼 생성

--- src/main/java/com/sns/server/reply/Reply.java
+++ src/main/java/com/sns/server/reply/Reply.java
@@ -5,9 +5,12 @@ import com.sns.server.post.Post;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "reply")
@@ -28,4 +31,13 @@ public class Reply {
 
     @Column(columnDefinition = "TEXT")
     private String content;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime created;
+
+    @LastModifiedDate
+    private LocalDateTime updated;
+
+    private LocalDateTime deleted;
 }
