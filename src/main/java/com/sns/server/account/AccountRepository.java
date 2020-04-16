056a9bf1adf0f8bd71762fe64c0d2417554c3f8a
preciouStar <seongjin605@users.noreply.github.com>
2020-04-17 01:05:57 +0900

    Remove optional in account repository

--- src/main/java/com/sns/server/account/AccountRepository.java
+++ src/main/java/com/sns/server/account/AccountRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Scope("prototype")
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByEmail(String email);
+    Account findByEmail(String email);
 }
