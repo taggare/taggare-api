a5c12d678292cf602cc9cac1f07ead80997b8e9b
preciouStar <seongjin605@users.noreply.github.com>
2020-05-27 00:30:40 +0900

    오타 수정

--- src/main/java/com/sns/server/common/ApiResponse.java
+++ src/main/java/com/sns/server/common/ApiResponse.java
@@ -15,15 +15,15 @@ import java.util.List;
 @ApiModel
 public class ApiResponse {
 
-    @ApiModelProperty(notes = "요청에 대햔 결과 데이터(리스트일 경우)")
+    @ApiModelProperty(notes = "요청에 대 결과 데이터(리스트)")
     private List<?> list;
 
-    @ApiModelProperty(notes = "요청에 대햔 결과 데이터(단일 오브젝트인 경우)")
+    @ApiModelProperty(notes = "요청에 대한 결과 데이터(오브젝트)")
     private Object data;
 
-    @ApiModelProperty(notes = "응답 HTTP STATUS CODE가 2XX가 아닌 경우 에러에 관한 메세지")
+    @ApiModelProperty(notes = "HTTP response code가 2XX가 아닌, 에러에 관한 메세지")
     private String message;
 
-    @ApiModelProperty(notes = "응답 HTTP STATUS CODE")
+    @ApiModelProperty(notes = "HTTP response code")
     private HttpStatus status;
 }
