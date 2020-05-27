6efab41566a3b9bf90462b70550ece9a92abe891
preciouStar <seongjin605@users.noreply.github.com>
2020-05-27 23:05:22 +0900

    토큰 요청 cross origin 허용

--- src/main/java/com/sns/server/security/ResourceServerConfig.java
+++ src/main/java/com/sns/server/security/ResourceServerConfig.java
@@ -24,7 +24,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/users")
+                .antMatchers(HttpMethod.POST, "/users","/oauth/token")
                 .permitAll()
                 .expressionHandler(webExpressionHandler())
                 .anyRequest()
